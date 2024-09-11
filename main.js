cd92ebc9a0cf0c96bb8b29f0be76030acf0af1f2
DiegoPertierraM <diegopmenendez@gmail.com>
2024-09-11 20:47:44 +0200

    Add mobile responsive

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { initializePopUp } from './helpers/popup.js';
 
 window.addEventListener('scroll', onScroll);
 
-document.querySelector('.showCv').addEventListener('click', () => {
+document.querySelector('.show-cv').addEventListener('click', () => {
   window.open(
     'https://drive.google.com/drive/u/2/folders/16XkgexZRrCOHoBAuCWLtXSEzrfGN5KK_',
     '_blank'
